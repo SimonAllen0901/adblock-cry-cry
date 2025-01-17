0d1b7042fc7149ad33b25cb640b50f98022d8af5
SimonAllen <simonallen19930901@gmail.com>
2025-01-17 19:11:20 +0800

    feat: update methods of AdBlockCryCry class

--- index.js
+++ index.js
@@ -2,17 +2,42 @@ class AdBlockCryCry {
   constructor() {}
 
   init(callback) {
+    const dataContainer = document.createElement("div");
+    dataContainer.innerHTML = this.generatesHTMLString();
+    document.body.append(dataContainer);
     callback();
   }
 
   detect() {}
 
-  generatesHTMLString() {}
+  generatesHTMLString() {
+    return `
+      <div id="ads"></div>
+    `;
+  }
 
-  checkVisibilityHidden() {}
+  checkVisibilityHidden() {
+    const element = document.querySelector(`#ads`);
+    if (!element) return true;
+  }
 
-  checkBlockedResource() {}
+  checkBlockedResource() {
+    return new Promise((resolve) => {
+      const fakeAd = document.createElement("img");
+      fakeAd.src = `/ads.jpg`;
+      fakeAd.onerror = () => resolve(true);
+      fakeAd.onload = () => resolve(false);
+      document.body.appendChild(fakeAd);
+    });
+  }
 
-  checkBlockedRequests() {}
+  checkBlockedRequests() {
+    return new Promise((resolve) => {
+      const randomPath = `/ads.js`;
+      fetch(randomPath)
+        .then(() => resolve(false))
+        .catch(() => resolve(true));
+    });
+  }
 }
 export default AdBlockCryCry;
