86e9f18e382a7a180e265e189874f1c72ba644d3
SimonAllen <simonallen19930901@gmail.com>
2025-02-13 10:36:50 +0800

    test: add tests for AdBlockCryCry

--- index.test.js
+++ index.test.js
@@ -12,4 +12,25 @@ describe("AdBlockCryCry", () => {
     expect(detector).toBeInstanceOf(AdBlockCryCry);
     expect(detector.elementIds).toContain("AdHeader");
   });
+
+  test("Should execute init and call the callback", (done) => {
+    detector.init(() => {
+      // 若這段能成功執行代表 init 有成功執行 callback
+      expect(true).toBe(true);
+      done();
+    });
+  });
+
+  test("Should execute detect and return a boolean", async () => {
+    detector.init(async () => {
+      const isBlocked = await detector.detect();
+      expect(typeof isBlocked).toBe("boolean");
+    });
+  });
+
+  test("Should insert ad elements into document.body", () => {
+    detector.init();
+    const adElements = document.body.querySelectorAll("div");
+    expect(adElements.length).toBeGreaterThan(0);
+  });
 });
